1138a8d013abdf97cf919984af87f9540dffb72a
Yonathan Castillo <88.jonathan@gmail.com>
2020-07-30 08:22:24 -0700

    deleted the files that were not uptodate

---
+++ Capstone1/Capstone_part_1.py
@@ -0,0 +1,26 @@
+# This is part one code for Data Exploration.
+
+# import packages
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+# Load the Data
+
+filename = './1586540818_05152020_updated_ski_data_-_updated_ski_data.csv'
+info = pd.read_csv(filename)
+
+# Let us explore the dataset
+info.shape
+info.columns
+
+# Let us clean the dataset base on the information needed. 
+info = info.reset_index()
+info.head()
+
+# Rename the columns
+info.index
+info.head()
+info.columns
